05e79f5071d9b357a12e0fecd49e54145e120714
mmagagnin <mmagagninr@gmail.com>
2022-06-30 18:26:12 -0300

    melhoria deteccao vermelho + retorna matriz com posicao da bolinha

--- ENTREGA2-MARINA.py
+++ ENTREGA2-MARINA.py
@@ -5,45 +5,71 @@ stream=VideoCapture(0)
 while True:
     _, imagem = stream.read()
    
-    linhas = 7
+    linhas = 5
     colunas = 8
     tamanho = 60
    
+   
+   
+    #tabuleiro matriz
+    matriz=[[None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            ]
+
+#        
+    imagemHSV = cvtColor(imagem,COLOR_BGR2HSV)
+   
+    dCores={"vermelho":{"escuro":(0,100,70),"claro":(10,255,255)}}
+   
+#"amarelo":{{"escuro":(),"claro":()}},"azul":{{"escuro":(),"claro":()}},"verde":{{"escuro":(),"claro":()}},"rosa":{{"escuro":(),"claro":()}},"roxo":{{"escuro":(),"claro":()}}
+    for cor,dCor in dCores.items():
+        mascara = inRange(imagemHSV,dCor["escuro"],dCor["claro"])
+        imagem2 = bitwise_and(imagem, imagem, mask=mascara)
+        imshow("Janela1",imagem2)
+        contornos,_ = findContours(mascara,RETR_TREE,CHAIN_APPROX_SIMPLE)
+       
+       
+    #detecta quando uma bolinha vermelha entra no campo da camera
+        for contorno in contornos:
+            x,y,comprimento,altura = boundingRect(contorno)
+            if comprimento*altura>=3000:
+                coordx=(x-60)//60
+                coordy=(y-60)//60
+                if coordx<=8 and coordy<=5:
+                    rectangle(imagem,pt1=(x,y),pt2=(x+comprimento,y+altura),color=(240,250,70),thickness=2)
+                    #putText(imagem,("VERMELHO: Linha: "+str(coordy+1)+", Coluna: "+str(coordx+1)),(20,30),color=(0,0,255),fontFace=FONT_HERSHEY_SIMPLEX,fontScale=1,thickness=4)
+                    print(cor+" - Linha: "+str(coordy+1)+", Coluna: "+str(coordx+1))
+                    matriz[coordy][coordx]=cor
+                    print(matriz)
+
+       
     #desenhando tabuleiro
     i=0
-    x1=0
-    while i<linhas:
-        rectangle(imagem,pt1=(x1,0),pt2=(x1+tamanho,linhas*tamanho),color=(90,90,90),thickness=3)
+    x1=60
+    y1=60
+   
+    while i<colunas:
+        rectangle(imagem,pt1=(x1,y1),pt2=(x1+tamanho,(linhas+1)*tamanho),color=(90,90,90),thickness=3)
         x1+=tamanho
         i+=1
-       
+#        
     j=0
-    y1=0
-    while j<colunas-1:
-        rectangle(imagem,pt1=(0,y1),pt2=(colunas*tamanho,y1+tamanho),color=(90,90,90),thickness=3)
+    x1=60
+    y1=60
+    while j<linhas:
+        rectangle(imagem,pt1=(x1,y1),pt2=((colunas+1)*tamanho,y1+tamanho),color=(240,50,70),thickness=3)
         y1+=tamanho
         j+=1
-       
-    imagemHSV = cvtColor(imagem,COLOR_BGR2HSV)
-    laranja_escuro=(0,90,255)
-    laranja_claro=(0,230,255)
-    mascara = inRange(imagemHSV,laranja_escuro,laranja_claro)
-   
-    contornos,_ = findContours(mascara,RETR_TREE,CHAIN_APPROX_SIMPLE)
-   
-    #detecta quando uma bolinha laranja entra no campo da camera
-    for contorno in contornos:
-        x,y,comprimento,altura = boundingRect(contorno)
-        #print(x,y)
-        coordx=x//60
-        coordy=y//60
-        print("Coordenadas: ("+str(coordx+1)+","+str(coordy+1)+")")
-       
 
     imshow("Minha Janela",imagem)
    
+   
     if waitKey(1) & 0xFF == ord("q"):
         break
+   
    
 stream.release()
 destroyAllWindows()
--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 Objetivo do trabalho:
 Criar um tabuleiro de dimensões 5x8 que permita o encaixe de 1 bolinha colorida por célula. 
 As variáveis são:
-* Cada cor de bolinha representa um instrumento diferente
-* Cada uma das 5 fileiras representa uma nota musical
+* Cada uma das 7 cores de bolinha representa uma nota musical, a ser definida através de interface gerada ao 
+longo do projeto
+* Cada uma das 5 fileiras representa um instrumento musical diferente.
 * Cada uma das 8 colunas representa um tempo na batida da música.
 Uma câmera suspensa fará a leitura por coluna do tabuleiro, e o programa deverá processar os sons adequados. 
 Uma fita de LEDs abaixo do tabuleiro fará a iluminação expositiva de cada bolinha em sua respectiva célula,
