d0898724f072180e07c9c701944f11bc39b971db
mmagagnin <mmagagninr@gmail.com>
2022-07-07 18:31:25 -0300

    transforming each coordinate into a dictionary

---
+++ operacoesmatriz.py
@@ -0,0 +1,60 @@
+
+def vInicialDic(matriz):
+    lDic=[]
+    i=0
+    for linha in matriz:
+        lDic.append([])
+        for posicao in linha:
+            lDic[i].append({"corAntiga":None,"corAtual":None,"contagem":0})
+        i+=1
+    return lDic
+
+def atualizaContagemDic(d):
+    if d["corAntiga"]==d["corAtual"]:
+        d["contagem"]+=1
+        if d["contagem"]==5:
+            return True
+    else:
+        d["corAntiga"]=d["corAtual"]
+        d["contagem"]=0
+
+def atualizaLdeDic(matrizNova,lD):
+    linha=0
+    for row in matrizNova:
+        coluna=0
+        for bolinha in row:
+            lD[linha][coluna]["corAtual"]=matrizNova[linha][coluna]
+            atualizaContagemDic(lD[linha][coluna])
+            coluna+=1
+        linha+=1
+
+matriz=[[None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            ]
+
+dicInicial=vInicialDic(matriz)
+print(dicInicial)
+
+matrizNova=[['vermelho',None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            [None,None,None,None,None,None,None,None],
+            ]
+
+atualizaLdeDic(matrizNova,dicInicial)
+
+print(dicInicial)
+
+
+
+
+
+
+
+
+
+            
